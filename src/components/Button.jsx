557a0d4ab532bb91a193b388bf5f078b2ab2dfd3
SimonM <smor@doorloop.com>
2024-07-04 15:20:41 +0300

    Deploy

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Button = ({ styles, text = "Get Started", onClick }) => (
-  <button type="button" onClick={onClick} className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
-    {text}
-  </button>
-);
-
-export default Button;
